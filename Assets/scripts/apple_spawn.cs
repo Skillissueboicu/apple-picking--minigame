28927205a602713e8e1837173233a729c1efbfde
Alecsandru Boicu <teod0414@gmail.com>
2026-09-12 16:16:40 +0200

    project

--- Assets/scripts/apple_spawn.cs
+++ Assets/scripts/apple_spawn.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class apple_spawn : MonoBehaviour
 {
+    public GameObject Spawn;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+      Instantiate(Spawn);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    
     }
 }
