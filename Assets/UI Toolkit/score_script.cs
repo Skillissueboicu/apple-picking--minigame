aec4e8e939ad8a121df9b8a2bd7eacdb86a5ff5e
Alecsandru Boicu <teod0414@gmail.com>
2026-09-12 19:27:41 +0200

    actuelle

--- Assets/UI Toolkit/score_script.cs
+++ Assets/UI Toolkit/score_script.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
+using TMPro;
 
 public class score_script : MonoBehaviour
 {
-public TMP_Text scoreText;
-private int scoreCount=0;
+    public TMP_Text scoreText;
+    private int scoreCount = 0;
 
     public void AddScore(int scoreToAdd)
     {
-        scoreCount ++ ; scoreText.AddScore(scoreToAdd);
-        UpdateScore.Text= "SCORE: " + scoreCount.ToString();
-
+        scoreCount += scoreToAdd;
+        scoreText.text = "SCORE: " + scoreCount.ToString();
     }
 }
+
