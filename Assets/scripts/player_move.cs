a63683b7195d93ad54208cd8f7166122c085db22
Alecsandru Boicu <teod0414@gmail.com>
2026-09-14 17:11:14 +0200

    update

---
+++ Assets/scripts/player_move.cs
@@ -0,0 +1,40 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.Tilemaps;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private float direction;
+    private Rigidbody2D rigidbody2D;
+    private SpriteRenderer spriteRenderer;
+    private float Speed = 10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = new Vector3(-4.2f, 6f, 2.1f);
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dirrection = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            dirrection += new Vector3(2f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            dirrection += new Vector3(-2f, 0f, 0f);
+            spriteRenderer.flipX = true;
+        }
+
+        dirrection = dirrection.normalized * Speed;
+        transform.position += dirrection * Time.deltaTime;
+    }
+}
